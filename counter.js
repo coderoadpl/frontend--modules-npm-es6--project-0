233da7c83dc8b4faf774128ab89b392b5b7d9055
Mateusz Choma <chomamateusz@gmail.com>
2020-08-10 13:06:03 +0200

    Automatic commit

---
+++ counter.js
@@ -0,0 +1,8 @@
+console.log('Hello counter!')
+
+export let number = 0
+
+export const getNumber = () => number
+
+export const inc = () => number = number + 1
+export const dec = () => number = number - 1
